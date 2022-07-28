3c88c6e725b145399b10baff398a400bb1b752f6
Kundan <kundan kamble>
2022-07-28 16:57:38 -0700

    Added Welcome to Emp Wage program

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
