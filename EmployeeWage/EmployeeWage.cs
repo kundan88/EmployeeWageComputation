84f19d1aa324dcd562c8961fcd5d0e23dcc85cf1
Kundan <kundan kamble>
2022-07-28 23:49:32 -0700

    PartTime Employee program

---
+++ EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWage
+    {
+        public static void PartTimeEmployeeWage()
+        {
+            int wrkHr;
+            int empRatePrHr = 20;
+            int empWage;
+            int isFullTime = 1;
+            int isPresent = 1;
+
+            int employeeStatus = new Random().Next(0, 2);
+            int empTime = new Random().Next(0, 2);
+
+            if (employeeStatus == isPresent)
+            {
+                if (empTime == isFullTime)
+                {
+                    Console.WriteLine("Employee is Present");
+                    wrkHr = 8;
+
+                }
+                else
+                {
+                    Console.WriteLine("Employe is Present");
+                    wrkHr = 4;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                wrkHr = 0;
+            }
+
+            empWage = wrkHr * empRatePrHr;
+            Console.WriteLine("Employee wage is " + empWage);
+        }
+
+    }
+
+}
