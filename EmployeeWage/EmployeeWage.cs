c67f96c7180442a81d8952f38160caf80f3941f0
Kundan <kundan kamble>
2022-07-28 17:14:03 -0700

    Employee Presnt Absent program

---
+++ EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWage
+    {
+        public static void EmployeeCheckAttendance()
+        {
+            int is_Full_Time = 1;
+            Random random = new Random();
+            int employeeCheck = new Random().Next(2);
+
+            if (employeeCheck == is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
