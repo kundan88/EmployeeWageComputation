b75a200879b4eab58fa799ca59c337f99624b421
Kundan <kundan kamble>
2022-07-29 18:48:52 -0700

    Emp Wage Using Switch Case program

---
+++ EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWage
+    {
+        public const int empRatePrHr = 20;
+        public const int isFullTime = 2;
+        public const int isPartTime = 1;
+       
+        public static void EmpWageUsingSwitchCase()
+        {
+
+            int workHr;
+            int totalWage;
+
+            int employeeStatus = new Random().Next(2);
+
+            switch (employeeStatus)
+            {
+                case isFullTime:
+                    workHr = 8;
+                    break;
+                case isPartTime:
+                    workHr = 4;
+                    break;
+                default:
+                    workHr = 0;
+                    break;
+            }
+            totalWage = empRatePrHr * workHr;
+            Console.WriteLine("Employee total wage is " + totalWage);
+        }
+    }
+ }
+
+
+
